e05de638821b586cb286e77b3a73cd73a6d518c6
Lei <rockassical@gmail.com>
2024-04-08 11:39:59 -0400

    first commit

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	//add
+	Rigidbody2D rbody;
+
+	void FixedUpdate () 
+	{
+		bool jetpackActive = Input.GetButton("Fire1");
+		
+		if (jetpackActive)
+		{
+			//remove
+			//rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+			//replace with
+			rbody=GetComponent<Rigidbody2D>();
+			rbody.AddForce( new Vector2(0, jetpackForce));
+		}
+	}
+}
